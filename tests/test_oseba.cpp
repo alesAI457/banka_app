1b63b50e2c488169380bfc84fd892487769ff02f
ales <vaje.izobrazevanje@gmail.com>
2024-01-24 22:30:50 +0100

    update cmake and first test

---
+++ tests/test_oseba.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "Oseba.hpp"
+
+TEST(OsebaTest, GetIdOseba)
+{
+    Oseba oseba{"Marko", "Polo", 3};
+
+    EXPECT_EQ(oseba.getId(), 3);
+}
+
+TEST(OsebaTest, IzpisiPodatkeTest) {
+    // Ustvarimo objekt razreda Oseba za testiranje
+    Oseba oseba{"Marko", "Polo", 3};
+
+    // Pričakovani rezultat
+    std::stringstream expected_output;
+    expected_output << "ID: 3, Ime: Marko, Priimek: Polo" << std::endl;
+
+    // Izvedemo metodo za izpis podatkov
+    std::stringstream actual_output;
+    oseba.izpisiPodatke();
+    actual_output << testing::internal::GetCapturedStdout();
+
+    // Preverimo, ali je izhod pravilen
+    EXPECT_EQ(expected_output.str(), actual_output.str());
+}
